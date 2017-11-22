9164568f984e2b8a18a269611e360bf8b9f7d696
Christopher Smith <Chris.Software.Developer@gmail.com>
2017-11-22 13:43:50 -0800

    Added a third action to both IBActions

--- PopUps/Controllers/ViewController.swift
+++ PopUps/Controllers/ViewController.swift
@@ -17,11 +17,15 @@ class ViewController: UIViewController {
         let action1 = UIAlertAction(title: "My Action 1", style: .default) { (action) in
             print("THIS IS ACTION 1")
         }
-        let action2 = UIAlertAction(title: "My Action 2", style: .default) { (action) in
+        let action2 = UIAlertAction(title: "My Action 2", style: .cancel) { (action) in
                 print("THIS IS ACTION 2")
+        }
+        let action3 = UIAlertAction(title: "My Action 3", style: .destructive) { (action) in
+            print("DELETING ALL YOUR DATA")
         }
         alert.addAction(action1)
         alert.addAction(action2)
+        alert.addAction(action3)
         present(alert, animated: true, completion: nil)
     }
     
@@ -30,11 +34,13 @@ class ViewController: UIViewController {
         let action1 = UIAlertAction(title: "My Action 1", style: .default) { (action) in
             print("THIS IS ACTION 1")
         }
-        let action2 = UIAlertAction(title: "My Action 2", style: .default) { (action) in
-            print("THIS IS ACTION 2")
+        let action2 = UIAlertAction(title: "My Action 2", style: .cancel, handler: nil)
+        let action3 = UIAlertAction(title: "BAD STUFF", style: .destructive) { (_) in
+            print("YOU HAVE DELETED YOUR ACCOUNT")
         }
         sheet.addAction(action1)
         sheet.addAction(action2)
+        sheet.addAction(action3)
         present(sheet, animated: true, completion: nil)
     }
     
