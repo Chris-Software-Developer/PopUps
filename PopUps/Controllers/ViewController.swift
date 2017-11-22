0f618bf7194dfc998aaf8c519f314439c636043a
Christopher Smith <Chris.Software.Developer@gmail.com>
2017-11-22 13:34:44 -0800

    Added action logic to IBActions to dismiss alerts

--- PopUps/Controllers/ViewController.swift
+++ PopUps/Controllers/ViewController.swift
@@ -14,11 +14,19 @@ class ViewController: UIViewController {
     
     @IBAction func alertButtonPressed(_ sender: UIButton) {
         let alert = UIAlertController(title: "My title", message: "My message", preferredStyle: .alert)
+        let action1 = UIAlertAction(title: "My Action 1", style: .default) { (action) in
+            print("THIS IS ACTION 1")
+        }
+        alert.addAction(action1)
         present(alert, animated: true, completion: nil)
     }
     
     @IBAction func actionSheetButtonPressed(_ sender: UIButton) {
         let sheet = UIAlertController(title: "My title", message: "My message" , preferredStyle: .actionSheet)
+        let action1 = UIAlertAction(title: "My Action 1", style: .default) { (action) in
+            print("THIS IS ACTION 1")
+        }
+        sheet.addAction(action1)
         present(sheet, animated: true, completion: nil)
     }
     
