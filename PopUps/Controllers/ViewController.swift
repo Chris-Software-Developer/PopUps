631d19b8be5722b374525908b3e1daf1603510bb
Christopher Smith <Chris.Software.Developer@gmail.com>
2017-11-22 13:26:38 -0800

    Added logic for IBActions

--- PopUps/Controllers/ViewController.swift
+++ PopUps/Controllers/ViewController.swift
@@ -13,8 +13,14 @@ class ViewController: UIViewController {
     // MARK: IBActions
     
     @IBAction func alertButtonPressed(_ sender: UIButton) {
+        let alert = UIAlertController(title: "My title", message: "My message", preferredStyle: .alert)
+        present(alert, animated: true, completion: nil)
     }
     
     @IBAction func actionSheetButtonPressed(_ sender: UIButton) {
+        let sheet = UIAlertController(title: "My title", message: "My message" , preferredStyle: .actionSheet)
+        present(sheet, animated: true, completion: nil)
     }
+    
+    
 }
